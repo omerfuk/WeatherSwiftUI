cd90192df2497e87899f4caa3bf26d806596b6d5
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-20 08:37:53 +0300

    Initial Commit

---
+++ WeatherUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 20.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
